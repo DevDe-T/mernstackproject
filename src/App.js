8bda1933db6d66d50858c47db610d7e2a5f4a911
Dev-De <chaudharydeepak47667@gmail.com>
2023-05-11 20:03:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/signup"
+					element={<Signup />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+	return (
+		<>
+			<div className="home">
+				<h1 className="title">Hello Mern developer</h1>
+				<div className="home1"></div>
+				<div className="home2"></div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
